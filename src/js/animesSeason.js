a41e2732e3fbb9d850f771770d29ac6da0cbb075
Wallace Santana <wallacesantanak0@gmail.com>
2024-07-05 21:17:28 -0300

    ATUALIZAÇÂO 2.0
    Adicionei diversas funcionalidades novas, alem da atualizacao da temporada de verao.

---
+++ src/js/animesSeason.js
@@ -0,0 +1,116 @@
+const animesSegunda = {
+    conteudo1:{
+        tituloBanner: "Tsue to Tsurugi no Wistoria",
+        imagemBanner: "https://www.anitube.vip/media/categories/video/866102370852bb9a9b29f88245582302.jpg",
+        subtituloBanner: "Estúdio: Actas, Bandai Namco Pictures\nGêneros: Ação Aventura Fantasia",
+        linkBanner: "https://www.anitube.vip/anime/tsue-to-tsurugi-no-wistoria"
+    },
+    conteudo2:{
+        tituloBanner: "Isekai Shikkaku",
+        imagemBanner: "https://www.anitube.vip/media/categories/video/2b8dc9b28cd4b481d10366eca2f8f107.jpg",
+        subtituloBanner: "Estúdio: AtelierPontdarc\nGêneros: Aventura Comédia Fantasia",
+        linkBanner: "https://www.anitube.vip/anime/isekai-shikkaku"
+    }
+}
+
+const animesTerca = {
+    conteudo1: {
+        imagemBanner: "https://www.anitube.vip/media/categories/video/98c6cad52e53452af0a74bfb43ca34f6.jpg",
+        tituloBanner: "Tokidoki Bosotto Russia-go",
+        subtituloBanner: "Estúdio: Doga Kobo\nGêneros: Comédia Romance",
+        linkBanner: "https://www.anitube.vip/anime/tokidoki-bosotto-russia-go-de-dereru-tonari-no-alya-san"
+    },
+    conteudo2: {
+        imagemBanner: "https://www.anitube.vip/media/categories/video/137cc046a03a23d40de605447c0467dd.jpg",
+        tituloBanner: "Megami no Café Terrace",
+        subtituloBanner: "Estúdio: Tezuka Productions\nGêneros: Comédia Ecchi Romance",
+        linkBanner: "https://www.anitube.vip/anime/megami-no-cafe-terrace-2nd-season"
+    }
+}
+
+const animesQuarta = {
+    conteudo1: {
+        tituloBanner: "Naze Boku no Sekai",
+        imagemBanner: "https://www.anitube.vip/media/categories/video/14cdda52cd28c8ef05e2e04fa45c87f1.jpg",
+        subtituloBanner: "Estúdio: Project No.9\nGêneros: Ação Fantasia Sci-Fi",
+        linkBanner: "https://www.anitube.vip/anime/naze-boku-no-sekai-wo-daremo-oboeteinai-no-ka"
+    },
+    conteudo2: {
+        tituloBanner: "Maougun Saikyou",
+        imagemBanner: "https://www.anitube.vip/media/categories/video/b4f622b0137725556084f36695822ee8.jpg",
+        subtituloBanner: "Estúdio: Studio A-CAT Productions\nGêneros: Ação Aventura Fantasia",
+        linkBanner: "https://www.anitube.vip/anime/maougun-saikyou-no-majutsushi-wa-ningen-datta"
+    }
+};
+
+const animesQuinta = {
+    conteudo1: {
+        tituloBanner: "Hazurewaku no Joutai Ijou Skill",
+        imagemBanner: "https://www.anitube.vip/media/categories/video/9548e90265f38ead95821af5f3b8e443.jpg",
+        subtituloBanner: "Estúdio: Seven Arcs\nGêneros: Ação Aventura Fantasia",
+        linkBanner: "https://www.anitube.vip/anime/hazurewaku-no-joutai-ijou-skill-de-saikyou-ni-natta-ore-ga-subete-wo-juurin-suru-made"
+    },
+    conteudo2: {
+        tituloBanner: "Shinmai Ossan Boukensha",
+        imagemBanner: "https://www.anitube.vip/media/categories/video/4a8b4c4f67e42507a12653ddec7f803d.jpg",
+        subtituloBanner: "Estúdio: Yumeta Company\nGêneros: Ação Aventura Fantasia",
+        linkBanner: "https://www.anitube.vip/anime/shinmai-ossan-boukensha-saikyou-party-ni-shinu-hodo-kitaerarete-muteki-ni-naru"
+    }
+};
+
+const animesSexta = {
+    conteudo1: {
+        tituloBanner: "Ore wa Subete wo Parry suru",
+        imagemBanner: "https://www.anitube.vip/media/categories/video/7b97a2649f020f4f783a633a2a5dad77.jpg",
+        subtituloBanner: "Estúdio: OLM\nGêneros: Ação Aventura Fantasia",
+        linkBanner: "https://www.anitube.vip/anime/ore-wa-subete-wo-parry-suru-gyaku-kanchigai-no-sekai-saikyou-wa-boukensha-ni-naritai"
+    },
+    conteudo2: {
+        tituloBanner: "Mob kara Hajimaru",
+        imagemBanner: "https://www.anitube.vip/media/categories/video/78204df7624cd0d745e33d3e3b0ab4b5.jpg",
+        subtituloBanner: "Estúdio: Gekkou\nGêneros: Ação Aventura Fantasia",
+        linkBanner: "https://www.anitube.vip/anime/mob-kara-hajimaru-tansaku-eiyuutan"
+    }
+};
+const animesSabado = {
+    conteudo1: {
+        tituloBanner: "Tower of God Season 2",
+        imagemBanner: "https://www.anitube.vip/media/categories/video/1b3be97f332334a9fab0311966efebb5.jpg",
+        subtituloBanner: "Estúdio: The Answer Studio\nGêneros: Ação Aventura Drama Fantasia Mistério",
+        linkBanner: "https://www.anitube.vip/anime/kami-no-tou-ouji-no-kikan"
+    },
+    conteudo2: {
+        tituloBanner: "Fairy Tail 100nen quest",
+        imagemBanner: "https://www.anitube.vip/media/categories/video/02e49ba702b18b9e3d4b9a026c66981b.jpg",
+        subtituloBanner: "Estúdio: J.C.Staff\nGêneros: Ação Aventura Fantasia",
+        linkBanner: "https://www.anitube.vip/anime/fairy-tail-100-nen-quest"
+    }
+};
+const animesDomingo = {
+    conteudo1: {
+        tituloBanner: "One Piece🏴‍☠️",
+        imagemBanner: "https://pbs.twimg.com/media/F_0mcEtWEAAVPCm?format=jpg&name=900x900",
+        subtituloBanner: "Autor: Eiichiro Oda\nDireção: Konosuke Uda\nEstúdio: Toei Animation\nStatus: Infinito\nGêneros: Ação Aventura Comédia Drama Fantasia Shounen Superpoder",
+        linkBanner: "https://www.anitube.vip/anime/398a5cebfadb2606fbf802b42aad57755b4eae55"
+    },
+    conteudo2: {
+        tituloBanner: "NieR:Automata Ver1.1a Part 2",
+        imagemBanner: "https://www.anitube.vip/media/categories/video/f1cd74018ae0d7502faf660d7141bb65.jpg",
+        subtituloBanner: "Estúdio: A-1 Pictures\nGêneros: Ação Fantasia Sci-Fi",
+        linkBanner: "https://www.anitube.vip/anime/nier-automata-ver1-1a-part-2"
+    }
+};
+const animesExtra = {
+    conteudo1: {
+        tituloBanner: "(Des)encanto",
+        imagemBanner: "https://image.tmdb.org/t/p/w342/yWLGhF6Rvv7oWC9ozzO0rvfQwYt.jpg",
+        subtituloBanner: "Gênero: Animação Aventura Comédia",
+        linkBanner: "https://comandotorrents.to/desencanto-1a-temporada-completa-torrent-2018-dual-audio-web-dl-720p-download"
+    },
+    conteudo2: {
+        tituloBanner: "KENGAN ASHURA",
+        imagemBanner: "https://www.anitube.vip/media/categories/video/1b64e998d51c7d69647c0722e5bd797c.jpg",
+        subtituloBanner: "Estúdio: Larx Entertainment\nGêneros: Ação Artes Marciais CGI",
+        linkBanner: "https://www.anitube.vip/anime/kengan-ashura-season-2-part-2"
+    }
+};
---
+++ src/js/season.js
@@ -0,0 +1,194 @@
+//Pegando as estacoes do ano
+function getSeason() {
+    const date = new Date();
+    const year = date.getFullYear();
+
+    // Definição das datas de início e término das estações
+    const seasons = [
+        { name: "inverno", start: new Date(year, 11, 21), end: new Date(year, 2, 20) },  // Inverno: 21 de Dez - 20 de Mar
+        { name: "primavera", start: new Date(year, 2, 21), end: new Date(year, 5, 20) },   // Primavera: 21 de Mar - 20 de Jun
+        { name: "verao", start: new Date(year, 5, 21), end: new Date(year, 8, 20) },   // Verão: 21 de Jun - 20 de Set
+        { name: "outono", start: new Date(year, 8, 21), end: new Date(year, 11, 20) }   // Outono: 21 de Set - 20 de Dez
+    ];
+
+    // Ajuste para a última data do inverno
+    seasons[0].end.setFullYear(year + 1);
+
+    for (let season of seasons) {
+        if (date >= season.start && date <= season.end) {
+            return season.name;
+        }
+    }
+    // Caso a data não corresponda a nenhuma estação (fica com o padrao)
+    return 'padrao';
+}
+
+
+let num
+function emojiTemp(emoji){
+    let emojiTemp = ["❄️","🌸", "☀️","🍁"]
+    
+    if (emoji >= 0 && emoji < emojiTemp.length) {
+        return emojiTemp[emoji];
+    } else {
+        // Caso o índice seja inválido, retorna um valor padrão (ou vazio, dependendo do caso)
+        return "";
+    }
+}
+
+
+function runSeason(season){
+    let conteudoBack = document.querySelector("#conteudo")
+    let conteudoH1 = document.querySelector("section#conteudo  h1")
+    let conteudoH2 = document.querySelectorAll("section#conteudo  h2")
+    let conteudoP = document.querySelectorAll('section#conteudo  p');
+
+    const seasons = {
+        defaultSeason:{
+            backgroundImage: 'src/img/backDef.png',
+            backgroundConteudo: 'linear-gradient(to bottom, #6210afcb,#4b12f7,#4207f5, #16074a00)',
+            backgroundh1: '#16074a7c',
+            backgroundh2: 'linear-gradient(to top, #4000ff, #16074adc)',
+            backgroundp: 'linear-gradient(to top, #4b12f7, #16074a0a )'
+        },
+        winterSeason:{
+            backgroundImage: 'src/img/backEstacoes/winter.png',
+            backgroundConteudo: 'linear-gradient(to bottom, #024873, #03658C, #88A5BF, rgba(22, 7, 74, 0))',
+            backgroundh1: '#73482F',
+            backgroundh2: 'linear-gradient(180deg, rgba(2,72,115,1) 0%, rgba(3,101,140,1) 57%, rgba(2,72,115,0.8911939775910365) 92%)',
+            backgroundp: 'linear-gradient(180deg, rgba(2,72,115,0.7595413165266106) 0%, rgba(3,101,140,0.9500175070028011) 57%, rgba(2,72,115,1) 92%)'
+        },
+        springSeason:{
+            backgroundImage:"src/img/backEstacoes/spring.jpg",
+            backgroundConteudo: 'linear-gradient(rgb(191, 46, 33) 0%, rgb(242 174 199 / 38%) 75%, rgb(242 206 216 / 0%) 100%), rgb(74 7 7 / 0%)',
+            backgroundh1: '#95261d',
+            backgroundh2: 'linear-gradient(180deg, rgba(191,46,33,1) 37%, rgba(187,112,105,0.8407738095238095) 89%)',
+            backgroundp: 'linear-gradient(180deg, rgba(242,109,120,0.9248074229691877) 23%, rgba(191,46,33,1) 67%)'
+        },
+        summerSeason:{
+            backgroundImage:"src/img/backEstacoes/summer.jpg",
+            backgroundConteudo: 'linear-gradient(#a64812 0%, #f28f16fc 56%, #f2a51600 100%)',
+            backgroundh1: '#95261d',
+            backgroundh2: 'linear-gradient(180deg, rgba(166,72,18,1) 0%, rgba(242,165,22,0.9023984593837535) 100%)',
+            backgroundp: 'linear-gradient(180deg, rgba(242,165,22,0.9500175070028011) 0%, rgba(166,72,18,1) 100%)'
+        },
+        autumnSeason:{
+            backgroundImage:"src/img/backEstacoes/autumn.jpg",
+            backgroundConteudo: 'linear-gradient(180deg, rgba(166,72,18,1) 0%, rgba(242,165,22,0.9500175070028011) 71%, rgba(209,124,20,0.3841911764705882) 95%, rgba(209,124,20,0.3841911764705882) 100%)',
+            backgroundh1: '#95261d',
+            backgroundh2: 'linear-gradient(180deg, rgba(166,72,18,1) 0%, rgba(242,165,22,0.9500175070028011) 62%, rgba(209,124,20,0.17130602240896353) 100%)',
+            backgroundp: 'linear-gradient(180deg, rgba(182,80,22,0.865983893557423) 0%, rgba(213,146,22,0.9500175070028011) 62%, rgba(209,124,20,0.227328431372549) 100%)'
+        }
+    }
+
+    if(season == 'padrao'){
+        document.body.style.background = "url(" + seasons.defaultSeason.backgroundImage + ") center bottom";
+        conteudoBack.style.background = seasons.defaultSeason.backgroundConteudo;
+        conteudoH1.style.background = seasons.defaultSeason.backgroundh1;
+        conteudoH2.forEach(titulo => {
+            titulo.style.background = seasons.defaultSeason.backgroundh2
+        })
+        conteudoP.forEach(paragrafo => {
+            paragrafo.style.background = seasons.defaultSeason.backgroundp;
+        });
+
+    }else if(season == 'inverno'){
+        document.body.style.background = "url(" + seasons.winterSeason.backgroundImage + ") center center";
+        num = 0
+
+        conteudoBack.style.background = "url(src/img/efects/neve.png) center center," + seasons.winterSeason.backgroundConteudo;
+
+        conteudoH1.style.background = seasons.winterSeason.backgroundh1
+        
+        conteudoH2.forEach(titulo => {
+            titulo.style.background = seasons.winterSeason.backgroundh2
+        })
+        conteudoP.forEach(paragrafo => {
+            paragrafo.style.background = seasons.winterSeason.backgroundp;
+        });
+    }else if(season == 'primavera'){
+        document.body.style.background = 'url(' + seasons.springSeason.backgroundImage + ') center center';
+        
+        num = 1
+    
+        conteudoBack.style.background = "url(src/img/efects/cherry.png) center center," + seasons.springSeason.backgroundConteudo;
+        conteudoH1.style.background = seasons.springSeason.backgroundh1
+        
+        conteudoH2.forEach(titulo => {
+            titulo.style.background = seasons.springSeason.backgroundh2
+        })
+        conteudoP.forEach(paragrafo => {
+            paragrafo.style.background = seasons.springSeason.backgroundp;
+        });
+    }else if(season == 'verao'){
+        document.body.style.background = 'url(' + seasons.summerSeason.backgroundImage + ') center top';
+        num = 2
+        conteudoH1.setHTMLUnsafe
+        conteudoBack.style.background = "url(src/img/efects/gira.png) left center," + seasons.summerSeason.backgroundConteudo;
+        
+        conteudoH1.style.background = seasons.summerSeason.backgroundh1
+        conteudoH2.forEach(titulo => {
+            titulo.style.background = seasons.summerSeason.backgroundh2
+        })
+        conteudoP.forEach(paragrafo => {
+            paragrafo.style.background = seasons.summerSeason.backgroundp;
+        });
+    }else if(season == 'outono'){
+        document.body.style.background = 'url(' + seasons.autumnSeason.backgroundImage + ') center center';
+        num = 3
+        conteudoBack.style.background = "url(src/img/efects/autum.png) center center," + seasons.autumnSeason.backgroundConteudo;
+        
+
+        conteudoH1.style.background = seasons.autumnSeason.backgroundh1
+        
+        conteudoH2.forEach(titulo => {
+            titulo.style.background = seasons.autumnSeason.backgroundh2
+        })
+        conteudoP.forEach(paragrafo => {
+            paragrafo.style.background = seasons.autumnSeason.backgroundp;
+        })
+    }
+}
+
+document.addEventListener("DOMContentLoaded", runSeason(getSeason()))
+console.log(getSeason())
+
+
+//mudar o tema clicando no numero 1, 2, 3, 4 ou 5
+document.addEventListener('keydown', function(event) {
+    switch (event.key) {
+        case '1':
+            runSeason('padrao')
+            break;
+        case '2':
+            runSeason('inverno')
+            break;
+        case '3':
+            runSeason('primavera')
+            break
+        case '4':
+            runSeason('verao')
+            break
+        case '5':
+            runSeason("outono")
+            break
+        default:
+            break;
+    }
+}); 
+
+
+//Funcao que dar pause ou play na radio de fundo
+function playRadio(event){
+    if(event.code == 'Space'){
+        event.preventDefault();
+        let audio = document.getElementById('radio');
+        if(audio.paused){
+            audio.play();
+        }else{
+            audio.pause();
+        }
+    }
+}
+
+document.addEventListener('keydown', playRadio)
