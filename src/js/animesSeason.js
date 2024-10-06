f78e6e94d21ab2443709486d1af70dbe0576f117
Wallace Santana <wallacesantanak0@gmail.com>
2024-10-06 19:14:26 -0300

    Att banner

--- src/js/animesSeason.js
+++ src/js/animesSeason.js
@@ -1,19 +1,19 @@
 const animesSegunda = {
     conteudo1:{
         tituloBanner: "Hitoribocchi no Isekai Kouryaku",
-        imagemBanner: "https://www.anitube.us/media/categories/video/c1e82f6215cd78dfaf51d40d537bc6d4.jpg",
+        imagemBanner: "https://www.intoxianime.com/wp-content/uploads/2022/06/Hitoribocchi-no-Isekai-Kouryaku.jpg",
         subtituloBanner: "Estúdio: Passione, Hayabusa Film\nGêneros: Ação Aventura Fantasia",
         linkBanner: "https://anitube.us/anime/hitoribocchi-no-isekai-kouryaku"
     },
     conteudo2:{
         tituloBanner: "Youkai Gakkou no Sensei Hajimemashita!",
-        imagemBanner: "https://www.anitube.us/media/categories/video/6320e78b71829ed711ec9cfbd234307d.jpg",
+        imagemBanner: "https://animenew.com.br/wp-content/uploads/2024/08/GVF3aBcaEAMQGlE-jpg.webp",
         subtituloBanner: "Estúdio: Satelight\nGêneros: Comédia Fantasia",
         linkBanner: "https://www.anitube.us/anime/youkai-gakkou-no-sensei-hajimemashita"
     },
     conteudo3:{
         tituloBanner: "Kamonohashi Ron no Kindan Suiri 2nd ",
-        imagemBanner: "https://www.anitube.us/media/categories/video/9b5ff551172607fa0ca7eaad23514c7e.jpg",
+        imagemBanner: "https://cdn.myanimelist.net/images/about_me/ranking_items/8987777-61bb5e22-b583-418d-8980-1a5f7d9a3e0f.jpg",
         subtituloBanner: "Estúdio: Diomedéa\nGêneros: Comédia Mistério",
         linkBanner: "https://www.anitube.us/anime/kamonohashi-ron-no-kindan-suiri-2nd-season"
     }
@@ -22,13 +22,13 @@ const animesSegunda = {
 const animesTerca = {
     conteudo1: {
         tituloBanner: "Kimi wa Meido-sama.",
-        imagemBanner: "https://www.anitube.us/media/categories/video/52402ec7cb6e5abfd7f416b37bbf8a85.jpg",
+        imagemBanner: "https://www.intoxianime.com/wp-content/uploads/2024/03/GJH3AxCb0AAEavX.jpeg",
         subtituloBanner: "Estúdio: Felix Film\nGêneros: Comédia",
         linkBanner: "https://www.anitube.us/anime/kimi-wa-meido-sama"
     },
     conteudo2: {
         tituloBanner: "Kekkon suru tte, Hontou desu ka",
-        imagemBanner: "https://www.anitube.us/media/categories/video/5af968913bec59add1662306f13606f9.jpg",
+        imagemBanner: "https://m.media-amazon.com/images/I/81hB4wDRTtL._AC_UF1000,1000_QL80_.jpg",
         subtituloBanner: "Estúdio: Ashi Productions\nGêneros: Romance",
         linkBanner: "https://www.anitube.us/anime/kekkon-suru-tte-hontou-desu-ka"
     }
@@ -37,13 +37,13 @@ const animesTerca = {
 const animesQuarta = {
     conteudo1: {
         tituloBanner: "Maou 2099",
-        imagemBanner: "https://www.anitube.us/media/categories/video/c09d8e37054eaab9b2fe4924356ff53e.jpg",
+        imagemBanner: "https://m.media-amazon.com/images/M/MV5BNzdiYjQ1OGYtYTJlNC00YjlkLTg1MzEtNWRiYzlhMTZlZjgwXkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg",
         subtituloBanner: "Estúdio: J.C.Staff\nGêneros: Ação Fantasia Sci-Fi",
         linkBanner: "https://www.anitube.us/anime/maou-2099"
     },
     conteudo2: {
         tituloBanner: "Shangri-La Frontier 2nd",
-        imagemBanner: "https://www.anitube.us/media/categories/video/e510e5861dc6136e2ef1c0c873db0a92.jpg",
+        imagemBanner: "https://a.storyblok.com/f/178900/1061x1500/5a05a2792f/shangri-la-frontier-season-2-key-visual.jpg",
         subtituloBanner: "Estúdio: C2C\nGêneros: Ação Aventura Fantasia",
         linkBanner: "https://www.anitube.us/anime/shangri-la-frontier-kusoge-hunter-kamige-ni-idoman-to-su-2nd-season"
     }
@@ -52,13 +52,13 @@ const animesQuarta = {
 const animesQuinta = {
     conteudo1: {
         tituloBanner: "Sayounara Ryuusei, Konnichiwa Jinsei",
-        imagemBanner: "https://www.anitube.us/media/categories/video/e0096a3bcfa5a2ac9901775766a18bd2.jpg",
+        imagemBanner: "https://i0.wp.com/www.otakupt.com/wp-content/uploads/2024/03/Sayounara-Ryuusei-Konnichiwa-Jinsei-anime-1st-visual.jpg?resize=696%2C1048&ssl=1",
         subtituloBanner: "Estúdio: SynergySP, Vega Entertainment\nGêneros: Ação Aventura Fantasia",
         linkBanner: "https://www.anitube.us/anime/sayounara-ryuusei-konnichiwa-jinsei"
     },
     conteudo2: {
         tituloBanner: "Nageki no Bourei wa Intai shitai",
-        imagemBanner: "https://www.anitube.us/media/categories/video/663f1f4e4e78504f5c9ee584b97fe14c.jpg",
+        imagemBanner: "https://i3.wp.com/irisscanlator.com.br/wp-content/uploads/2023/03/fad9f5b6-58a3-42e9-9f22-2e8c0b5cd513.jpg",
         subtituloBanner: "Estúdio: Zero-G\nGêneros: Ação Aventura Fantasia",
         linkBanner: "https://www.anitube.us/anime/nageki-no-bourei-wa-intai-shitai"
     }
@@ -67,13 +67,13 @@ const animesQuinta = {
 const animesSexta = {
     conteudo1: {
         tituloBanner: "Mecha-Ude: Mechanical Arms",
-        imagemBanner: "https://www.anitube.us/media/categories/video/bbc36605e481523edc715abfd22d272b.jpg",
+        imagemBanner: "https://animesinjapan.com.br/envios/2024/03/27/4dc60dcee38e3069d3392b3009683931.jpg",
         subtituloBanner: "Estúdio: OLM\nGêneros: Ação Sci-Fi",
         linkBanner: "https://www.anitube.us/anime/mecha-ude-tv"
     },
     conteudo2: {
         tituloBanner: "Blue Exorcist: Beyond the Snow Saga",
-        imagemBanner: "https://www.anitube.us/media/categories/video/76d3bac70e82dafb18692a2af93cf32c.jpg",
+        imagemBanner: "https://m.media-amazon.com/images/M/MV5BMDMzNTc0N2YtYjE4Yy00NTQzLThjN2UtOTk2OWE0ZTY5NTI1XkEyXkFqcGc@._V1_FMjpg_UX1000_.jpg",
         subtituloBanner: "Estúdio: Studio VOLN\nGêneros: Ação Aventura Fantasia",
         linkBanner: "https://www.anitube.us/anime/ao-no-exorcist-yuki-no-hate-hen"
     }
@@ -81,13 +81,13 @@ const animesSexta = {
 const animesSabado = {
     conteudo1: {
         tituloBanner: "DAN DA DAN",
-        imagemBanner: "https://www.anitube.us/media/categories/video/5eeec20c3fb193da9cbc38d2f8a4303c.jpg",
+        imagemBanner: "https://universojapanesemusic.com/wp-content/uploads/2024/09/dan-da-dan.webp?w=816",
         subtituloBanner: "Estúdio: Science SARU\nGêneros: Ação Comédia Sobrenatural",
         linkBanner: "https://www.anitube.us/anime/dandadan"
     },
     conteudo2: {
         tituloBanner: "Blue Lock 2nd Season",
-        imagemBanner: "https://www.anitube.us/media/categories/video/25f9519f238e4b059c7e36de2f98baa1.jpg",
+        imagemBanner: "https://www.intoxianime.com/wp-content/uploads/2024/08/GVPsEcebIAAPJCQ-724x1024.jpg",
         subtituloBanner: "Estúdio: 8bi\nGêneros: Esportes",
         linkBanner: "https://www.anitube.us/anime/blue-lock-vs-u-20-japan"
     }
@@ -95,13 +95,13 @@ const animesSabado = {
 const animesDomingo = {
     conteudo1: {
         tituloBanner: "One Piece🏴‍☠️",
-        imagemBanner: "https://pbs.twimg.com/media/F_0mcEtWEAAVPCm?format=jpg&name=900x900",
+        imagemBanner: "https://assets.gamearena.gg/wp-content/uploads/2023/07/29231448/One-Piece-Vol-106.jpg.webp",
         subtituloBanner: "Autor: Eiichiro Oda\nDireção: Konosuke Uda\nEstúdio: Toei Animation\nStatus: Infinito\nGêneros: Ação Aventura Comédia Drama Fantasia Shounen Superpoder",
-        linkBanner: "https://www.anitube.vip/anime/398a5cebfadb2606fbf802b42aad57755b4eae55"
+        linkBanner: "https://www.anitube.vip/anime/807a39145f9b086e788f33c9e1cd52197ff1ee33"
     },
     conteudo2: {
         tituloBanner: "Bleach: Sennen Kessen-hen - Soukoku-tan",
-        imagemBanner: "https://www.anitube.us/media/categories/video/1aa939cf35391b2c75fe0fe4f15b9375.jpg",
+        imagemBanner: "https://www.animenewsnetwork.com/hotlink/images/encyc/A30131-1764180711.1720315342.jpg",
         subtituloBanner: "Estúdio: Pierrot Films\nGêneros: Ação Aventura Fantasia",
         linkBanner: "https://www.anitube.us/anime/bleach-sennen-kessen-hen-soukoku-tan"
     }
